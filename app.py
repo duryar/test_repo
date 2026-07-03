699dcb723b76bad4a85a0b992c1d8288323d622f
duryar <duryar01@roberthalf.com>
2026-07-03 12:27:57 +0530

    Add GET API endpoint /api/get

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 # This is Flask application
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello from Flask inside Docker!"
+
+@app.route('/api/get', methods=['GET'])
+def get_data():
+    return jsonify({"message": "This is a GET API endpoint", "status": "success"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
