a315edd51582562710d48142c6e83b9f9ece9c49
duryar <duryar01@roberthalf.com>
2026-07-03 12:46:37 +0530

    Delete routes directory

--- routes/get_api.py
+++
@@ -1,6 +0,0 @@
-from flask import jsonify
-
-def register_get_routes(app):
-    @app.route('/api/get', methods=['GET'])
-    def get_data():
-        return jsonify({"message": "This is a GET API endpoint", "status": "success"})
--- routes/post_api.py
+++
@@ -1,7 +0,0 @@
-from flask import request, jsonify
-
-def register_post_routes(app):
-    @app.route('/api/post', methods=['POST'])
-    def post_data():
-        data = request.get_json()
-        return jsonify({"message": "Data received successfully", "data": data, "status": "success"})
