23a3814795e2cd7266897abbe65a50da6552b1d1
duryar <duryar01@roberthalf.com>
2026-07-03 12:44:53 +0530

    Consolidate routes and APIs into app.py - remove routes folder dependencies

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 # This is Flask application
+# Example code in starting of script
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Home route
 @app.route('/')
 def home():
     return "Hello from Flask inside Docker!"
 
+# GET API endpoint - consolidated from routes/get_api.py
 @app.route('/api/get', methods=['GET'])
 def get_data():
     return jsonify({"message": "This is a GET API endpoint", "status": "success"})
 
+# POST API endpoint - consolidated from routes/post_api.py
 @app.route('/api/post', methods=['POST'])
 def post_data():
     data = request.get_json()
